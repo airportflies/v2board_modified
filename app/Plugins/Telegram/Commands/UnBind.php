6edd812ba20938b47b66e51de3b65e31d8a6ae0d
Elysia <heinu333@gmail.com>
2025-01-25 22:16:54 +0800

    支持admin通过命令指定解绑某个用户tgid

--- app/Plugins/Telegram/Commands/UnBind.php
+++ app/Plugins/Telegram/Commands/UnBind.php
@@ -11,7 +11,18 @@ class UnBind extends Telegram {
 
     public function handle($message, $match = []) {
         if (!$message->is_private) return;
-        $user = User::where('telegram_id', $message->chat_id)->first();
+        if (!isset($message->args[0])) {
+            $user = User::where('telegram_id', $message->chat_id)->first();
+        } else {
+            $chat = User::where('telegram_id', $message->chat_id)->first();
+            if (!$chat) return;
+            if (!($chat->is_admin || $chat->is_staff)) return;
+            if (strpos($message->args[0], '@') !== true) {
+                $user = User::where('email', $message->args[0])->first();
+            } else {
+                $user = User::where('telegram_id', $message->args[0])->first();
+            }
+        }
         $telegramService = $this->telegramService;
         if (!$user) {
             $telegramService->sendMessage($message->chat_id, '没有查询到您的用户信息，请先绑定账号', 'markdown');
